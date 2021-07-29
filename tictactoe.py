ff5ab32b5143cc1399ac082ef2baea3db21cd26b
ClaraShade <grzebyk.monika@gmail.com>
2021-07-29 20:58:17 +0200

    Picture of the board created as the matrix of lists

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,8 @@
 class Board:
-    def __init__(self, x):
-        self.length = x
-        self.squares = x**2
-
+    def __init__(self, side):
+        self.length = side
+        self.squares = side**2
+        self.picture = []
 class Square:
     def __init__(self, num, col, row):
         self.num = num
@@ -10,21 +10,29 @@ class Square:
         self.row = row
         self.is_empty = True
 
-    def mark(self, char):
+    def mark_x(self, char):
+        self.is_empty = False
+        self.char = 'cross'
+
+    def mark_o(self, char):
         self.is_empty = False
         self.char = 'cross'
 
 def createboard():
-    new_board = Board(3)
+    side = int(input("How wide board you want to create? Please, type natural number: "))
+    new_board = Board(side)
+    row_list = []
     square_index = 1
     for column in range (1,new_board.length+1):
+        row_list.append(" ")
         for row in range(1, new_board.length+1):
             new_square = Square(square_index, column, row)
-            square_index = square_index+1
             print(new_square.num, new_square.col, new_square.row)
-    return new_board
-
+            if square_index < new_board.squares:
+                square_index = square_index+1
+        new_board.picture.append(row_list)
+    print(square_index)
+    print(side)
+    for i in new_board.picture:
+        print(i)
 board = createboard()
-
-print(board.length)
-print(board.squares)
