84d68808e32cf9255024ee4775a6bde350f29187
ClaraShade <grzebyk.monika@gmail.com>
2021-08-05 20:36:31 +0200

    squares created and stores in board picture; square representation defined

--- tictactoe.py
+++ tictactoe.py
@@ -11,7 +11,11 @@ class Square:
         self.col = col
         self.row = row
         self.is_empty = True
-        self.char = ' '
+        self.char = '_'
+
+
+    def __repr__(self):
+        return self.char
 
     def mark_x(self):
         self.is_empty = False
@@ -25,20 +29,22 @@ class Square:
 def createboard():
     side = int(input("How wide board you want to create? Please, type natural number: "))
     new_board = Board(side)
-    row_list = []
     square_index = 1
     for column in range(1, new_board.length+1):
-        row_list.append(" ")
+        row_list = []
         for row in range(1, new_board.length+1):
             new_square = Square(square_index, column, row)
-            print(new_square.num, new_square.col, new_square.row)
+            print(new_square.num, new_square.col, new_square.row, new_square)
             if square_index < new_board.squares:
                 square_index = square_index+1
+            row_list.append(new_square)
         new_board.picture.append(row_list)
+
     print("Congratulations! You created " + str(side) + "-sided board, which has " + str(square_index) + " squares.")
     print("See your board below:")
-    for i in new_board.picture:
-        print(i)
+    return new_board
 
 
 board = createboard()
+for i in board.picture:
+    print(i)
